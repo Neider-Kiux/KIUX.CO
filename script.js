7a10fc04bbbc771d5ec5d31703f5cea27049c208
Neider-Kiux <ngarzon@kiux.com>
2023-12-13 17:17:14 -0500

    Pagina funcional y con buenos estilos

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 window.onresize = resolucion;
 window.onload = resolucion;
 
-var slideIndex = 1;
+let grupo = document.getElementsByClassName(`tarjetas-slider`);
+
+var slideIndex = [1, 1, 1, 1, 1];
 function resolucion() {
-    var ancho = window.innerWidth;
-    var alto = window.innerHeight;
-    if (ancho <= 1600) {
-        showDivs(slideIndex);
-    } else {
-        var x =
-            document.getElementsByClassName("mySlides-paraQuien");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "flex";
-        }
-    }
+	var ancho = window.innerWidth;
+	var alto = window.innerHeight;
+	for (i = 0; i < grupo.length; i++) {
+		if (ancho <= 1600) {
+			showDivs(slideIndex[i], i);
+		} else {
+			var x = document.getElementsByClassName(`mySlides-${i}`);
+			for (y = 0; y < x.length; y++) {
+				x[y].style.display = "flex";
+			}
+		}
+	}
 }
 
-function plusDivs(n) {
-    showDivs((slideIndex += n));
+function plusDivs(n, ubicacion) {
+	showDivs((slideIndex[ubicacion] += n), ubicacion);
 }
 
-function currentDiv(n) {
-    showDivs((slideIndex = n));
+function currentDiv(n, ubicacion) {
+	showDivs((slideIndex[ubicacion] = n), ubicacion);
 }
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides-paraQuien");
-    var dots = document.getElementsByClassName("ubicacion");
-    if (n > x.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("ls-red", "");
-    }
-    x[slideIndex - 1].style.display = "flex";
-    dots[slideIndex - 1].className += "ls-red";
+function showDivs(n, ubicacion) {
+	var i;
+	var x = document.getElementsByClassName(`mySlides-${ubicacion}`);
+	var dots = document.getElementsByClassName(`ubicacion-${ubicacion}`);
+	if (n > x.length) {
+		slideIndex[ubicacion] = 1;
+	}
+	if (n < 1) {
+		slideIndex[ubicacion] = x.length;
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace("ls-red", "");
+	}
+	x[slideIndex[ubicacion] - 1].style.display = "flex";
+	dots[slideIndex[ubicacion] - 1].className += "ls-red";
 }
